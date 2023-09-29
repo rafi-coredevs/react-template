d3e397911683846781bca11c240be768ab730dac
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-09-29 17:56:25 +0600

    Initial commit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import logo from '../../assets/Icons/react-logo.svg'
+import { Link } from 'react-router-dom';
 // import styles from './footer.module.css'
-
+const date = new Date();
 
 const index = () => {
   return (
@@ -9,20 +11,26 @@ const index = () => {
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
+            <Link to="/" className="flex items-center">
+              <img src={logo} className="h-8 mr-3" alt="FlowBite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">React Template</span>
+            </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                  <Link to="/" className="hover:underline">Link 1</Link>
                 </li>
-                <li>
-                  <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                <li className="mb-4">
+                  <Link to="/" className="hover:underline">Link 2</Link>
+                </li>
+                <li className="mb-4">
+                  <Link to="/" className="hover:underline">Link 3</Link>
+                </li>
+                <li className="mb-4">
+                  <Link to="/" className="hover:underline">Link 4</Link>
                 </li>
               </ul>
             </div>
@@ -30,10 +38,10 @@ const index = () => {
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                  <Link to="/" target='_blank' className="hover:underline">Github</Link>
                 </li>
                 <li>
-                  <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                  <Link to="/" target='_blank' className="hover:underline">Discord</Link>
                 </li>
               </ul>
             </div>
@@ -41,10 +49,10 @@ const index = () => {
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="#" className="hover:underline">Privacy Policy</a>
+                  <Link to="/" className="hover:underline">Github</Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                  <Link to="/" className="hover:underline">Github</Link>
                 </li>
               </ul>
             </div>
@@ -52,7 +60,7 @@ const index = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {date.getFullYear()} <a href="https://flowbite.com/" className="hover:underline">React Template</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
             <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
